f8e8f61cbe4dc9ae6726f8c187d78dd936289799
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-03 11:07:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,9 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |planeteer|
-    planeteer = 
+    planeteer = "#{planeteer.capitalize!}!"
+  end
+  return planeteer_calls
 end
 
 def long_planeteer_calls# code an argument here
